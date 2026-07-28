bffb6ced4b67c01a57f0d27258c800dd2b952221
isla <isla@local>
2026-07-28 15:05:16 +0800

    fix: retire legacy site caches

---
+++ vue-app/public/sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', () => {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    const cacheNames = await caches.keys()
+    await Promise.all(cacheNames.map(cacheName => caches.delete(cacheName)))
+    await self.clients.claim()
+
+    const clients = await self.clients.matchAll({
+      type: 'window',
+      includeUncontrolled: true,
+    })
+
+    await self.registration.unregister()
+    await Promise.all(clients.map(client => client.navigate(client.url)))
+  })())
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request))
+})
